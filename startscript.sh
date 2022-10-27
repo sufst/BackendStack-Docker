32290459fa13983811ec7d15026a08454fa6dffc
AndreasDemenagas <ademenagas01@gmail.com>
2022-10-27 18:35:01 +0100

    Updated to new Dockerfile

--- startscript.sh
+++ startscript.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Runs both servers in the backround
-python /var/intermediate-server/server.py &
-python /var/back-end/server.py &
+# source /var/intermediate-server/inter-server-venv/bin/activate && python server.py &
+# source /var/intermediate-server/backend-venv/bin/activate && python server.py &
+
+python3 /intermediate-server/server.py &
+python3 /back-end/server.py &
 
 # Stops if there is a crash or exit
 wait -n
